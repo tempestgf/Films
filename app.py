8336c523efb4a9f44aca85a616cd55de3b1ca1eb
Tempestgf <tempestgf@protonmail.com>
2024-05-02 21:38:21 +0200

    flask v2

--- app.py
+++ app.py
@@ -16,6 +16,67 @@ mydb = mysql.connector.connect(
 def index():
     return render_template('index.html')
 
+@app.route('/serie')
+def serie():
+    return render_template('daredevil.html')
+
+@app.route('/serieuser')
+def serieuser():
+    return render_template('daredeviluser.html')
+
+@app.route('/resenas')
+def resenas():
+    series = [
+        {
+            'nombre': 'Narcos',
+            'temporadas': ['Temporada 1', 'Temporada 2', 'Temporada 3'],
+            'reseñas': {
+                'Temporada 1': 'Narcos nos introduce en el mundo del narcotráfico colombiano de manera magistral. Las actuaciones son excelentes y la trama es emocionante.',
+                'Temporada 2': 'La segunda temporada de Narcos mantiene la calidad de la primera. La historia se vuelve aún más intensa y llena de giros inesperados.',
+                'Temporada 3': 'Aunque la tercera temporada de Narcos sigue siendo interesante, algunos fans consideran que no alcanza el nivel de las anteriores. Aún así, es una serie que merece la pena ver.'
+            }
+        },
+        {
+            'nombre': 'Stranger Things',
+            'temporadas': ['Temporada 1', 'Temporada 2', 'Temporada 3'],
+            'reseñas': {
+                'Temporada 1': 'Stranger Things nos transporta a la década de los 80 con su atmósfera nostálgica y su emocionante historia. Los personajes son carismáticos y la trama es intrigante desde el primer episodio.',
+                'Temporada 2': 'La segunda temporada de Stranger Things expande el universo de la serie y profundiza en la mitología establecida en la primera temporada. Los nuevos personajes y los giros argumentales mantienen el interés del espectador.',
+                'Temporada 3': 'Aunque la tercera temporada de Stranger Things es visualmente impresionante y llena de momentos emocionantes, algunos críticos consideran que la trama se vuelve un poco predecible en comparación con las temporadas anteriores.'
+            }
+        }
+    ]
+    return render_template('resenas.html', series=series)
+
+@app.route('/ver_resena', methods=['POST'])
+def ver_resena():
+    serie = request.form['serie']
+    temporada = request.form['temporada']
+    series = [
+        {
+            'nombre': 'Narcos',
+            'temporadas': ['Temporada 1', 'Temporada 2', 'Temporada 3'],
+            'reseñas': {
+                'Temporada 1': 'Excelente serie',
+                'Temporada 2': 'Muy recomendada',
+                'Temporada 3': 'Aún no hay reseña'
+            }
+        },
+        {
+            'nombre': 'Stranger Things',
+            'temporadas': ['Temporada 1', 'Temporada 2', 'Temporada 3'],
+            'reseñas': {
+                'Temporada 1': 'Muy entretenida',
+                'Temporada 2': 'Gran elenco de actores',
+                'Temporada 3': 'Esperando reseñas'
+            }
+        }
+    ]
+    reseña = series[0]['reseñas'][temporada] if serie == 'Narcos' else series[1]['reseñas'][temporada]
+    return render_template('resenas.html', serie=serie, temporada=temporada, reseña=reseña)
+
+
+
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
     if request.method == 'POST':
@@ -106,7 +167,7 @@ def login():
 
         if user:
             session['username'] = user[1]
-            return redirect(url_for('index'))
+            return render_template('indexuser.html')
         else:
             flash('Nom d\'usuari o contrasenya incorrecta', 'error')
             return render_template('login.html')
